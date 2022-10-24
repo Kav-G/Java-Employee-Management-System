0c7cf59f54a548297f8ed557d82899c93141a953
KavindhuG <kavidhuhasith@gmail.com>
2022-10-24 08:01:48 +0530

    Create Update Search

--- EmployeeManagementSystem/src/Employee.java
+++ EmployeeManagementSystem/src/Employee.java
@@ -94,5 +94,77 @@ public class Employee {
                 }
             }
         });
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+                    String empid = txtid.getText();
+
+                    pst = con.prepareStatement("select empname,salary,mobile from employee where id = ?");
+                    pst.setString(1, empid);
+                    ResultSet rs = pst.executeQuery();
+
+                    if(rs.next()==true)
+                    {
+                        String empname = rs.getString(1);
+                        String emsalary = rs.getString(2);
+                        String emmobile = rs.getString(3);
+
+                        txtName.setText(empname);
+                        txtSalary.setText(emsalary);
+                        txtMobile.setText(emmobile);
+
+                    }
+                    else
+                    {
+                        txtName.setText("");
+                        txtSalary.setText("");
+                        txtMobile.setText("");
+                        JOptionPane.showMessageDialog(null,"Invalid Employee Id");
+
+                    }
+                }
+                catch (SQLException ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+
+        });
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String empid, empname, salary, mobile;
+
+                empname = txtName.getText();
+                salary = txtSalary.getText();
+                mobile = txtMobile.getText();
+                empid = txtid.getText();
+
+                try {
+                    pst = con.prepareStatement("update employee set empname = ?,salary = ?,mobile = ? where id = ?");
+                    pst.setString(1, empname);
+                    pst.setString(2, salary);
+                    pst.setString(3, mobile);
+                    pst.setString(4, empid);
+
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Record Updated!!!!!");
+                    table_load();
+                    txtName.setText("");
+                    txtSalary.setText("");
+                    txtMobile.setText("");
+                    txtName.requestFocus();
+                }
+
+                catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
+            }
+            
+        });
     }
 }
